7352fe840beba0f215a7584191833e3d27d7a9e0
MansoorP <103396101+Mansoor-P@users.noreply.github.com>
2023-05-06 23:15:03 +0530

    database connectivity

---
+++ src/mansoor/github/unimanage/ui/Conn.java
@@ -0,0 +1,20 @@
+package mansoor.github.unimanage.ui;
+
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+
+    Conn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///unimanage", "root", "M1ansoor@2001");
+            s = c.createStatement();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
